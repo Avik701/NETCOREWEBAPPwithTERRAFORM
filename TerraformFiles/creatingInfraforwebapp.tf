3b9ee023b1993cf508f5776cc5182185ef636d95
Avik701 <avikchatterjee701@gmail.com>
2019-10-27 10:32:34 -0700

    changes ith tf az login

--- TerraformFiles/creatingInfraforwebapp.tf
+++ TerraformFiles/creatingInfraforwebapp.tf
@@ -1,15 +1,5 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
 
-provider "azurerm" {    
-    subscription_id="${var.subscription_id}"
-    client_id="${var.client_id}"
-    client_secret="${var.client_secret}"
-    tenant_id="${var.tenant_id}"  
-}
-
+provider "azurerm" { }
 resource "azurerm_resource_group" "dev" {
     name="avikdemowebapptf"
     location = "West Europe" 
